a1d50db395d26329ae109ac22556abc73440b7fd
Ortiz Cota <josephortizcota@my.whccd.edu>
2020-01-27 19:36:38 -0800

    Answered all the questions in my lecture 05 activity.

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1.Define a variable “myGreeting” and place a string containing a generic greeting inside of it.
+
+let myGreeting = "Hello User!";
+
+// 2.Output the contents of “myGreeting” using console.log(...).
+
+console.log(myGreeting); // Hello User!
+
+// 3.Define a constant variable “pi” and place a number containing the value for Pi inside of it.
+
+const pi = 3.14;
+
+// 4.Attempt to re-assign “pi” to something else - what happens? Why?
+
+// pi = 1; Error since pi is a constant is cna only be assigned to one number.
+
+// 5.Define a new variable “myNumber” and place a number of your choice inside of it.
+
+let myNumber = 9;
+
+// 6.Call the “min” function inside of the “Math” object on both “myNumber” and “pi” - what is the result? Why?
+
+console.log(Math.min(myNumber, pi)); // 3.14 because the Math.min statement pulls the lowest number from the numbers provided.
